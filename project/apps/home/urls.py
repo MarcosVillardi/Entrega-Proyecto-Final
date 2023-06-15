6ee89c7988dc8ac6ebd7a659f8a721784ed43ee6
Marcos Villardi <villardimarcos@gmail.com>
2023-06-15 16:36:24 -0300

    Update, seguridad

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -1,14 +1,15 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.admin.views.decorators import staff_member_required
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path("about/", views.info, name='about'),   
-    path('login/', views.login_request, name="login"),
+    path('login/', LoginView.as_view(template_name='home/login.html'), name="login"),
     path('logout/', LogoutView.as_view(template_name='home/logout.html'), name="logout"),
-    path('register/', views.register_request, name="register"),
+    path('register/', staff_member_required(views.register_request), name="register"),
 
 ]
 urlpatterns += staticfiles_urlpatterns()
--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,31 +1,14 @@
 from django.shortcuts import render
 from . import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.admin.views.decorators import staff_member_required
 
 def index(request):
     return render(request, 'home/index.html')
 
 def info(request):
     return render(request, 'home/about.html')
-
-def login_request(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return render(request, 'home/index.html',)
-            else:
-                return render(request, 'home/login.html', {'form': form})
-        else:
-            return render(request, 'home/login.html', {'form': form})
-    form = AuthenticationForm()
-    return render(request, 'home/login.html', {'form': form})
-
+@staff_member_required
 def register_request(request):
       if request.method == 'POST':
             form = forms.CustomUserCreationForm(request.POST)
